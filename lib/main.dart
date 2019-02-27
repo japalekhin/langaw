39e217d6422537e55bfc9e527a3ce937505d3b4a
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-27 21:55:52 +0800

    set the game to fullscreen and portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {}
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+}
