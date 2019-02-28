a75c162cff754dbda441b8569276fbf9495173ad
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-28 09:45:35 +0800

    linked langawgame to main runApp the game's widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:langaw/langaw-game.dart';
 
 void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  LangawGame game = LangawGame();
+  runApp(game.widget);
 }
