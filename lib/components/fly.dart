efca8cb70e1c10c6ee1169a09346273d86ff9e09
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-28 17:41:12 +0800

    added fly paint instance variable

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -4,9 +4,12 @@ import 'package:langaw/langaw-game.dart';
 class Fly {
   final LangawGame game;
   Rect flyRect;
+  Paint flyPaint;
 
   Fly(this.game, double x, double y) {
     flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+    flyPaint = Paint();
+    flyPaint.color = Color(0xff6ab04c);
   }
 
   void render(Canvas c) {}
