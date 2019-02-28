bec08150398cf61937228cfd994d6fc2d6c6e2c7
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-28 09:19:09 +0800

    added langaw-game game class

---
+++ lib/langaw-game.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class LangawGame extends Game {
+  Size screenSize;
+
+  void render(Canvas canvas) {}
+
+  void update(double t) {}
+
+  void resize(Size size) {}
+}
