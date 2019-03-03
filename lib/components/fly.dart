d8494a42e5aeb56a45043a50ad41a4151b2063ea
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-03 09:28:43 +0800

    added isOffScreen flag

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -6,6 +6,7 @@ class Fly {
   Rect flyRect;
   Paint flyPaint;
   bool isDead = false;
+  bool isOffScreen = false;
 
   Fly(this.game, double x, double y) {
     flyRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
@@ -20,6 +21,9 @@ class Fly {
   void update(double t) {
     if (isDead) {
       flyRect = flyRect.translate(0, game.tileSize * 12 * t);
+      if (flyRect.top > game.screenSize.height) {
+        isOffScreen = true;
+      }
     }
   }
 
