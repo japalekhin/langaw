539abca41f433456197cb0369a9192d86e5be779
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-08 18:46:40 +0800

    added backyard component

---
+++ lib/components/backyard.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+
+class Backyard {
+  Sprite bgSprite;
+
+  Backyard() {
+    bgSprite = Sprite('bg/backyard.png');
+  }
+
+  void render(Canvas c) {}
+
+  void update(double t) {}
+}
