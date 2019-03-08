e70c9f23d1bed19540649ffcb0964149363580d7
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-08 17:18:31 +0800

    preloaded the images in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/flame.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,25 @@ void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  Flame.images.loadAll(<String>[
+    'bg/backyard.png',
+    'flies/agile-fly-1.png',
+    'flies/agile-fly-2.png',
+    'flies/agile-fly-dead.png',
+    'flies/drooler-fly-1.png',
+    'flies/drooler-fly-2.png',
+    'flies/drooler-fly-dead.png',
+    'flies/house-fly-1.png',
+    'flies/house-fly-2.png',
+    'flies/house-fly-dead.png',
+    'flies/hungry-fly-1.png',
+    'flies/hungry-fly-2.png',
+    'flies/hungry-fly-dead.png',
+    'flies/macho-fly-1.png',
+    'flies/macho-fly-2.png',
+    'flies/macho-fly-dead.png',
+  ]);
 
   LangawGame game = LangawGame();
   runApp(game.widget);
