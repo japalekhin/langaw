cbf09982ad30137e0338c2a63092b283b3e01123
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-08 20:37:06 +0800

    added link to langaw game instance

--- lib/components/backyard.dart
+++ lib/components/backyard.dart
@@ -1,10 +1,13 @@
 import 'dart:ui';
 import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
 
 class Backyard {
+  final LangawGame game;
   Sprite bgSprite;
+  Rect bgRect;
 
-  Backyard() {
+  Backyard(this.game) {
     bgSprite = Sprite('bg/backyard.png');
   }
 
