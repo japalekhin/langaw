92ac621e20f0bb735362bcb0e797e26bc442591c
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-09 19:53:55 +0800

    added fly movement to random locations added comments to code

--- lib/components/fly.dart
+++ lib/components/fly.dart
@@ -34,14 +34,27 @@ class Fly {
 
   void update(double t) {
     if (isDead) {
+      // make the fly fall
       flyRect = flyRect.translate(0, game.tileSize * 12 * t);
       if (flyRect.top > game.screenSize.height) {
         isOffScreen = true;
       }
     } else {
+      // flap the wings
       flyingSpriteIndex += 30 * t;
       if (flyingSpriteIndex >= 2) {
         flyingSpriteIndex -= 2;
+      }
+
+      // move the fly
+      double stepDistance = speed * t;
+      Offset toTarget = targetLocation - Offset(flyRect.left, flyRect.top);
+      if (stepDistance < toTarget.distance) {
+        Offset stepToTarget = Offset.fromDirection(toTarget.direction, stepDistance);
+        flyRect = flyRect.shift(stepToTarget);
+      } else {
+        flyRect = flyRect.shift(toTarget);
+        setTargetLocation();
       }
     }
   }
