c70c0dd8efbd959276996b2a193c0aa8911a2845
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-18 03:45:02 +0800

    added lost view file

---
+++ lib/views/lost-view.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+
+class LostView {
+  final LangawGame game;
+  Rect rect;
+  Sprite sprite;
+
+  LostView(this.game) {
+    rect = Rect.fromLTWH(
+      game.tileSize,
+      (game.screenSize.height / 2) - (game.tileSize * 5),
+      game.tileSize * 7,
+      game.tileSize * 5,
+    );
+    sprite = Sprite('bg/lose-splash.png');
+  }
+
+  void render(Canvas c) {
+    sprite.renderRect(c, rect);
+  }
+
+  void update(double t) {}
+}
