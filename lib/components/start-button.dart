0de1ac8b0718960ef18f3a48aef848075f2648c5
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-17 18:02:19 +0800

    added start-button component

---
+++ lib/components/start-button.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+
+class StartButton {
+  final LangawGame game;
+  Rect rect;
+  Sprite sprite;
+
+  StartButton(this.game) {}
+
+  void render(Canvas c) {}
+
+  void update(double t) {}
+
+  void onTapDown() {}
+}
