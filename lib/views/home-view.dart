4c174bacd8ccdf035f21861aa5dbd414f5ab8a4d
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-17 16:29:03 +0800

    added home view

---
+++ lib/views/home-view.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+
+class HomeView {
+  final LangawGame game;
+  Rect titleRect;
+  Sprite titleSprite;
+
+  HomeView(this.game) {}
+
+  void render(Canvas c) {}
+
+  void update(double t) {}
+}
