713d35faa455d10ed1081765a87c75015245e1d9
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-19 17:49:52 +0800

    added spawn controller

---
+++ lib/controllers/spawner.dart
@@ -0,0 +1,13 @@
+import 'package:langaw/langaw-game.dart';
+
+class FlySpawner {
+  final LangawGame game;
+
+  FlySpawner(this.game) {}
+
+  void start() {}
+
+  void killAll() {}
+
+  void update(double t) {}
+}
