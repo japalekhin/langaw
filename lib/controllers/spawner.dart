b03101746090095302b5ee02e2726f29223922a7
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-20 20:40:52 +0800

    added constants to spawner added killAll method added start method

--- lib/controllers/spawner.dart
+++ lib/controllers/spawner.dart
@@ -1,13 +1,26 @@
+import 'package:langaw/components/fly.dart';
 import 'package:langaw/langaw-game.dart';
 
 class FlySpawner {
   final LangawGame game;
+  final int maxSpawnInterval = 3000;
+  final int minSpawnInterval = 200;
+  final int intervalChange = 200;
+  final int maxFliesOnScreen = 7;
+  int currentInterval;
+  int nextSpawn;
 
   FlySpawner(this.game) {}
 
-  void start() {}
+  void start() {
+    killAll();
+    currentInterval = maxSpawnInterval;
+    nextSpawn = DateTime.now().millisecondsSinceEpoch + currentInterval;
+  }
 
-  void killAll() {}
+  void killAll() {
+    game.flies.forEach((Fly fly) => fly.isDead = true);
+  }
 
   void update(double t) {}
 }
