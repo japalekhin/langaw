9c654bf7b1898b21a44cb4149d75582fd5a2c95a
Japa Alekhin Llemos <jap@alekhin.io>
2019-03-24 20:36:10 +0800

    added help and credits button

---
+++ lib/components/credits-button.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+import 'package:langaw/view.dart';
+
+class CreditsButton {
+  final LangawGame game;
+  Rect rect;
+  Sprite sprite;
+
+  CreditsButton(this.game) {
+    rect = Rect.fromLTWH(
+      game.screenSize.width - (game.tileSize * 1.25),
+      game.screenSize.height - (game.tileSize * 1.25),
+      game.tileSize,
+      game.tileSize,
+    );
+    sprite = Sprite('ui/icon-credits.png');
+  }
+
+  void render(Canvas c) {
+    sprite.renderRect(c, rect);
+  }
+
+  void onTapDown() {
+    game.activeView = View.credits;
+  }
+}
---
+++ lib/components/help-button.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+import 'package:flame/sprite.dart';
+import 'package:langaw/langaw-game.dart';
+import 'package:langaw/view.dart';
+
+class HelpButton {
+  final LangawGame game;
+  Rect rect;
+  Sprite sprite;
+
+  HelpButton(this.game) {
+    rect = Rect.fromLTWH(
+      game.tileSize * .25,
+      game.screenSize.height - (game.tileSize * 1.25),
+      game.tileSize,
+      game.tileSize,
+    );
+    sprite = Sprite('ui/icon-help.png');
+  }
+
+  void render(Canvas c) {
+    sprite.renderRect(c, rect);
+  }
+
+  void onTapDown() {
+    game.activeView = View.help;
+  }
+}
