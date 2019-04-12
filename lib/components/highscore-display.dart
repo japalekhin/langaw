e0a631a0f294ad73ad57524c902cb00863e14b16
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-12 08:33:40 +0800

    added highscore display class

---
+++ lib/components/highscore-display.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+import 'package:flutter/painting.dart';
+import 'package:langaw/langaw-game.dart';
+
+class HighscoreDisplay {
+  final LangawGame game;
+  TextPainter painter;
+  TextStyle textStyle;
+  Offset position;
+
+  HighscoreDisplay(this.game) {
+    painter = TextPainter(
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    );
+
+    Shadow shadow = Shadow(
+      blurRadius: 3,
+      color: Color(0xff000000),
+      offset: Offset.zero,
+    );
+
+    textStyle = TextStyle(
+      color: Color(0xffffffff),
+      fontSize: 30,
+      shadows: <Shadow>[shadow, shadow, shadow, shadow],
+    );
+
+    position = Offset.zero;
+
+    updateHighscore();
+  }
+
+  void updateHighscore() {
+    int highscore = game.storage.getInt('highscore') ?? 0;
+
+    painter.text = TextSpan(
+      text: 'Highscore: ' + highscore.toString(),
+      style: textStyle,
+    );
+
+    painter.layout();
+
+    position = Offset(
+      game.screenSize.width - (game.tileSize * .25) - painter.width,
+      game.tileSize * .25,
+    );
+  }
+
+  void render(Canvas c) {
+    painter.paint(c, position);
+  }
+}
