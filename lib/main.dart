0ffb9a222d90b9415d40deaf4d1ea5da21b0790c
Japa Alekhin Llemos <jap@alekhin.io>
2019-04-18 06:47:31 +0800

    preloaded sfx audio files

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,26 @@ void main() async {
     'ui/start-button.png',
   ]);
 
+  Flame.audio.disableLog();
+  Flame.audio.loadAll(<String>[
+    'sfx/haha1.ogg',
+    'sfx/haha2.ogg',
+    'sfx/haha3.ogg',
+    'sfx/haha4.ogg',
+    'sfx/haha5.ogg',
+    'sfx/ouch1.ogg',
+    'sfx/ouch2.ogg',
+    'sfx/ouch3.ogg',
+    'sfx/ouch4.ogg',
+    'sfx/ouch5.ogg',
+    'sfx/ouch6.ogg',
+    'sfx/ouch7.ogg',
+    'sfx/ouch8.ogg',
+    'sfx/ouch9.ogg',
+    'sfx/ouch10.ogg',
+    'sfx/ouch11.ogg',
+  ]);
+
   LangawGame game = LangawGame(storage);
   runApp(game.widget);
 
